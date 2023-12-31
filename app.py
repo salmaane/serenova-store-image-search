5a5ffc8550ff8ab9cef26c8ef55b9d4c356a8b43
Salmane <elmourtajisalmane@gmail.com>
2023-12-31 09:06:10 +0100

    added templates with their routes in flask app

--- app.py
+++ app.py
@@ -3,7 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+
+@app.route('/shop-single')
+def shopSingle():
+    return render_template('shop-single.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
